652b0df8109e3feaa8e7d7359abc1cb7a9ceaca2
olawale ibrahim <ibrahimolawale580@gmail.com>
2022-07-26 00:26:45 +0100

    search bar is functional

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,33 @@
 import React, {useEffect, useState} from 'react';
 import {getList} from '../services/list';
 import './Home.css';
-import NavBar from '../NavBar/NavBar'
 
 export default  function Home(){
   const [list, setList] = useState([]);
+  const [showSearch, setSearch] = useState(false);
+  const [searchText, setSearchText] = useState('');
+  const [matches, setMatches] = useState(['no match found !']);
+
+  const showSearchResults = (e) => {
+    const wrapper = document.getElementById('search-wrapper');
+    setSearchText(e.target.value);
+    wrapper.requestFullscreen();
+    setSearch(true);
+  }
+  const hideSearchResults = () =>{
+    document.exitFullscreen();
+    setSearch(false);
+  }
+
+  useEffect(() =>{
+    let mounted = true;
+    if(mounted){
+      let filteredList = list.filter(i => i.name.includes(searchText));
+      filteredList != []?setMatches(filteredList) : setMatches(['no match found!']); 
+    }
+    return () => mounted =false;
+  },[searchText]);
+
   useEffect(()=>{
     let mounted = true;
     getList()
@@ -13,12 +36,31 @@ export default  function Home(){
           setList(items)
         }
        })
-   return () => mounted = false;
+    return () => mounted = false;
   },[]);
   return (
     <div id = 'home-wrapper'>
       <h2>What would you like to cook</h2>
-      <input id = 'search' placeholder = 'Search for a Recipe' />
+      <div id = 'search-wrapper'>
+        {showSearch && <span onClick = {hideSearchResults}>{'<'}</span>}
+        <input id = 'search' placeholder = 'Search for a Recipe' type = 'search' name = 'q' onChange = {(e) => showSearchResults(e)} onFocus = {(e) => showSearchResults(e)} onBlur = {hideSearchResults}/>
+        {showSearch 
+           &&
+             <div id = 'search-results'>
+               {
+                 matches.map(i =>(
+                   <div className = 'popular-food-wrapper' key = {i.name}>
+                     <img src = {i.imageURL} alt ='' />
+                     <div className = 'popular-food-details'>
+                       <h5>{i.name}</h5>
+                       <p>Chef Ibra</p>
+                       <p>{i.timers.reduce((a,b) => ~~a + ~~b)} mins</p>
+                     </div>
+                   </div>
+                ))}
+              </div>
+        }
+      </div>
       <div className = 'h-and-p-wrapper'>
         <h3>Todays pick</h3>
         <p className = 'see-all'> see all</p>
@@ -29,7 +71,7 @@ export default  function Home(){
             <div  className = 'food-details' key = {i.name}>
               <img src = {i.imageURL} alt ='' />
               <h5>{i.name}</h5>
-              <p>Chef samad</p>
+              <p>Chef Samad</p>
               <p>{i.timers.reduce((a,b) => ~~a + ~~b)} mins</p>
             </div>
           ))}
